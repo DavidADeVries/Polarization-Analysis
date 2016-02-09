69f8deafde4ab2d1e3628129062f42582e0d8a23
Frank Corapi <fcorapi@femto.nexus.uwaterloo.ca>
2016-02-09 18:31:42 -0500

    Fix to Coordinates not displaying properly
    added a helper function in order to convert coordinates into a string

---
+++ GUI/Helper Functions/coordsIntoString.m
@@ -0,0 +1,8 @@
+function coordsString = coordsIntoString(xValue, yValue)
+%Turns the coordinates into a displayable string
+    coordsString = ['[',num2str(xValue), ', ', num2str(yValue), ']'];
+end
+
+
+
+
