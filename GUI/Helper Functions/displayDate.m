7cb0e932d59f0dbb9c58ba50e7fe7adf2471f957
David DeVries <ddevries@uwaterloo.ca>
2016-03-02 11:57:24 -0500

    Post Merge Clean-up

--- GUI/Helper Functions/displayDate.m
+++
@@ -1,9 +0,0 @@
-function displayString = displayDate(serialDate)
-% displayDate
-% takes MATLAB serial date and outputs it as a string
-
-format = 'mmm dd, yyyy';
-displayString = datestr(serialDate, format);
-
-end
-
--- GUI/Helper Functions/removeFileExtension.m
+++
@@ -1,8 +0,0 @@
-function trimmedFilename = removeFileExtension(filename)
-%removeFileExtension
-% removes the .XXX from the end of a file
-
-[trimmedFilename, ~] = splitFileExtension(filename);
-
-end
-
