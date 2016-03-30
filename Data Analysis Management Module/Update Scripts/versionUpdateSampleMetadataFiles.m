90908eb7cd09eb218723f77379f18e467f753a5f
David DeVries <ddevries@uwaterloo.ca>
2016-03-30 08:57:32 -0400

    Metadata Update Scripts Added

--- Data Analysis Management Module/Update Scripts/versionUpdateSampleMetadataFiles.m
+++ Data Analysis Management Module/Update Scripts/versionUpdateSampleMetadataFiles.m
@@ -3,19 +3,19 @@ function [] = versionUpdateSampleMetadataFiles(projectPath, toPath)
 
 % ** READ IN METADATA FILE **
 
-% vars = load(makePath(projectPath, toPath, 'sample_metadata.mat'), Constants.METADATA_VAR);
-% metadata = vars.metadata;
-%     
-% % ** UPDATE REQUIRED INFORMATION **
-% 
-% 
-% 
-% % ** SAVE IT **
-% 
-% metadataFilename = SampleNamingConventions.METADATA_FILENAME;
-% saveToBackup = true;
-% 
-% saveObjectMetadata(metadata, projectPath, toPath, metadataFilename, saveToBackup);
+vars = load(makePath(projectPath, toPath, 'sample_metadata.mat'), Constants.METADATA_VAR);
+metadata = vars.metadata;
+    
+% ** UPDATE REQUIRED INFORMATION **
+
+metadata.source = []; % clear it, updated it and it's throwing odd errors
+
+% ** SAVE IT **
+
+metadataFilename = SampleNamingConventions.METADATA_FILENAME;
+saveToBackup = true;
+
+saveObjectMetadata(metadata, projectPath, toPath, metadataFilename, saveToBackup);
 
 % ** RECURSE ON NEXT LEVEL **
 
--- Data Analysis Management Module/Update Scripts/versionUpdateSessionMetadataFiles.m
+++ Data Analysis Management Module/Update Scripts/versionUpdateSessionMetadataFiles.m
@@ -3,22 +3,22 @@ function [] = versionUpdateSessionMetadataFiles(projectPath, toPath)
 
 % ** READ IN METADATA FILE **
 
-vars = load(makePath(projectPath, toPath, 'session_metadata.mat'), Constants.METADATA_VAR);
-metadata = vars.metadata;
-
-if isa(metadata, 'MicroscopeSession')
-    % ** UPDATE REQUIRED INFORMATION **
-    
-    metadata.bwPixelSizeMicrons = 0.16; 
-    metadata.rgbPixelSizeMicrons = 0.17;
-    
-    % ** SAVE IT **
-    
-    metadataFilename = SessionNamingConventions.METADATA_FILENAME;
-    saveToBackup = true;
-    
-    saveObjectMetadata(metadata, projectPath, toPath, metadataFilename, saveToBackup);
-end
+% vars = load(makePath(projectPath, toPath, 'session_metadata.mat'), Constants.METADATA_VAR);
+% metadata = vars.metadata;
+% 
+% if isa(metadata, 'MicroscopeSession')
+%     % ** UPDATE REQUIRED INFORMATION **
+%     
+%     metadata.bwPixelSizeMicrons = 0.16; 
+%     metadata.rgbPixelSizeMicrons = 0.17;
+%     
+%     % ** SAVE IT **
+%     
+%     metadataFilename = SessionNamingConventions.METADATA_FILENAME;
+%     saveToBackup = true;
+%     
+%     saveObjectMetadata(metadata, projectPath, toPath, metadataFilename, saveToBackup);
+% end
 
 
 
